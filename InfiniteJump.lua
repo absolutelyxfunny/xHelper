2ccb774b814a6a52e99e84a7d6bcc70346465b9e
absolutelyxfunny <163680308+absolutelyxfunny@users.noreply.github.com>
2024-03-17 00:21:18 +0500

    Create InfiniteJump.lua

---
+++ InfiniteJump.lua
@@ -0,0 +1,30 @@
+Controls:
+Reinject the script to toggle the infinite jump script on or off.
+Excute Lua "G.infinjump = true" to explicity turn the infinite jump script on
+Excute Lua "G.infinjump = false" to explicity turn the infinite jump script off
+]]
+
+--Toggles the infinite jump between on or off on every script run
+_G.infinjump = not _G.infinjump
+
+if _G.infinJumpStarted == nil then
+	--Ensures this only runs once to save resources
+	_G.infinJumpStarted = true
+	
+	--Notifies readiness
+	game.StarterGui:SetCore("SendNotification", {Title="WeAreDevs.net"; Text="xHelper InfJump ready!"; Duration=5;})
+
+	--The actual infinite jump
+	local plr = game:GetService('Players').LocalPlayer
+	local m = plr:GetMouse()
+	m.KeyDown:connect(function(k)
+		if _G.infinjump then
+			if k:byte() == 32 then
+			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+			humanoid:ChangeState('Jumping')
+			wait()
+			humanoid:ChangeState('Seated')
+			end
+		end
+	end)
+end
